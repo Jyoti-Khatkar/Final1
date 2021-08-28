65d8fc2ad1a1979d7fd12110533398b74b42d35a
Jyoti_Khatkar <jyotikhatkar54@gmail.com>
2021-08-29 01:22:38 +0530

    favicon and dropdown not work

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,12 +2,21 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+//=require jquery
 
-import Rails from "@rails/ujs"
+
+import Rails, { $ } from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+require("semantic-ui-sass")
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+
+import '@doabit/semantic-ui-sass'
+import 'stylesheets/semantic-ui.scss'
+import '@doabit/semantic-ui-sass/src/scss/semantic-ui.scss'
+$(document).on("turbolinks:load", () => $(".ui.dropdown").dropdown());
